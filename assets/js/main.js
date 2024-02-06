d3dddbca1161e81e6b89292827adf8483385d399
Premchand27 <131382703+Premchand27@users.noreply.github.com>
2024-02-06 22:04:29 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-/**
-* Template Name: Arsha - v4.3.0
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-=======
-<<<<<<< HEAD
 
->>>>>>> 0eede7378273387a2c73e4a25d001fa19b822078
 (function() {
   "use strict";
 
@@ -34,38 +24,10 @@
         selectEl.forEach(e => e.addEventListener(type, listener))
       } else {
         selectEl.addEventListener(type, listener)
-<<<<<<< HEAD
-=======
-=======
-(function() {
-  "use strict";
-
-  const select = (el, all = false) => {
-    el = el.trim();
-    if (all) {
-      return [...document.querySelectorAll(el)];
-    } else {
-      return document.querySelector(el);
-    }
-  }
-
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all);
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener));
-      } else {
-        selectEl.addEventListener(type, listener);
->>>>>>> 0ecd24dbbfa46862e2d012eb2bd638a7c744a01a
->>>>>>> 0eede7378273387a2c73e4a25d001fa19b822078
       }
     }
   }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0eede7378273387a2c73e4a25d001fa19b822078
   /**
    * Easy on scroll event listener 
    */
@@ -254,157 +216,13 @@
   /**
    * Initiate portfolio lightbox 
    */
-<<<<<<< HEAD
-=======
-=======
-  const scrollto = (el) => {
-    let header = select('#header');
-    let offset = header.offsetHeight;
-    let elementPos = select(el).offsetTop;
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    });
-  }
-
-  const headerScrolled = () => {
-    let selectHeader = select('#header');
-    if (selectHeader) {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled');
-      } else {
-        selectHeader.classList.remove('header-scrolled');
-      }
-    }
-  }
-
-  const toggleBacktotop = () => {
-    let backtotop = select('.back-to-top');
-    if (backtotop) {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active');
-      } else {
-        backtotop.classList.remove('active');
-      }
-    }
-  }
-
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    let navbarlinks = select('#navbar .scrollto', true);
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active');
-      } else {
-        navbarlink.classList.remove('active');
-      }
-    });
-  }
-
-  const preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
-
-  window.addEventListener('load', () => {
-    navbarlinksActive();
-    headerScrolled();
-    toggleBacktotop();
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
-    });
-  });
-
-  window.addEventListener('scroll', () => {
-    headerScrolled();
-    toggleBacktotop();
-    navbarlinksActive();
-  });
-
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault();
-      let navbar = select('#navbar');
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile');
-        let navbarToggle = select('.mobile-nav-toggle');
-        navbarToggle.classList.toggle('bi-list');
-        navbarToggle.classList.toggle('bi-x');
-      }
-      scrollto(this.hash);
-    }
-  }, true);
-
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile');
-    this.classList.toggle('bi-list');
-    this.classList.toggle('bi-x');
-  });
-
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault();
-      this.nextElementSibling.classList.toggle('dropdown-active');
-    }
-  }, true);
-
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash);
-      }
-    }
-  });
-
-  let portfolioContainer = select('.portfolio-container');
-  if (portfolioContainer) {
-    let portfolioIsotope = new Isotope(portfolioContainer, {
-      itemSelector: '.portfolio-item'
-    });
-
-    on('click', '#portfolio-flters li', function(e) {
-      e.preventDefault();
-      let portfolioFilters = select('#portfolio-flters li', true);
-      portfolioFilters.forEach(function(el) {
-        el.classList.remove('filter-active');
-      });
-      this.classList.add('filter-active');
-
-      portfolioIsotope.arrange({
-        filter: this.getAttribute('data-filter')
-      });
-      portfolioIsotope.on('arrangeComplete', function() {
-        AOS.refresh();
-      });
-    }, true);
-  }
-
->>>>>>> 0ecd24dbbfa46862e2d012eb2bd638a7c744a01a
->>>>>>> 0eede7378273387a2c73e4a25d001fa19b822078
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
-<<<<<<< HEAD
   /**
    * Portfolio details slider
    */
-=======
-<<<<<<< HEAD
-  /**
-   * Portfolio details slider
-   */
-=======
->>>>>>> 0ecd24dbbfa46862e2d012eb2bd638a7c744a01a
->>>>>>> 0eede7378273387a2c73e4a25d001fa19b822078
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -419,10 +237,6 @@
     }
   });
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0eede7378273387a2c73e4a25d001fa19b822078
   /**
    * Animation on scroll
    */
@@ -435,25 +249,4 @@
     });
   });
 
-<<<<<<< HEAD
 })()
-=======
-})()
-=======
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
-
-})();
->>>>>>> 0ecd24dbbfa46862e2d012eb2bd638a7c744a01a
->>>>>>> 0eede7378273387a2c73e4a25d001fa19b822078
